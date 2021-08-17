da04b5a0b95c526a336dba521c2ee4f16967a22a
Luis Fernando Ascencion Romero <lascenci@everis.com>
2021-08-17 13:49:32 -0500

    Proyecto II

---
+++ src/main/java/com/spring/msprofile/entity/SubType.java
@@ -0,0 +1,24 @@
+package com.spring.msprofile.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.Valid;
+
+@Data
+@Builder
+@Document("SubType")
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubType {
+    @Id
+    String id;
+
+    @Valid
+    EnumSubType value;
+
+    enum EnumSubType{
+        NORMAL, VIP, PYME
+    }
+}
